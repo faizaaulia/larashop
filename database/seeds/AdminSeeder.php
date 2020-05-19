dabb6fbe05c8fcc26c25216c776264faadb13d41
faizaaulia <faizaaulia20@gmail.com>
2020-05-19 23:07:48 +0700

    users table migration & user admin seeding
    edit .htaccess

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new \App\User;
+        $admin->username = 'admin';
+        $admin->name = 'Site Administrator';
+        $admin->email = 'admin@larashop.test';
+        $admin->roles = json_encode(['ADMIN']);
+        $admin->password = \Hash::make('admin');
+        $admin->avatar = 'no-photo.jpg';
+        $admin->phone = '-';
+        $admin->address = 'Kepanjen, Malang';
+
+        $admin->save();
+
+        $this->command->info('admin user inserted');
+    }
+}
