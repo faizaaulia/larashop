92b11923445fae011b9ab09c17a0fe37b145cf1e
faizaaulia <faizaaulia20@gmail.com>
2020-05-20 13:54:11 +0700

    modify routing default route & delete register

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
+
+// hapus route register
+Route::match(['get', 'post'], '/register', function () {
+    return redirect('/login');
+})->name('register');
 
 Route::get('/home', 'HomeController@index')->name('home');
